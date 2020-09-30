2ef99e6dc2eca4d7e8f41f89057605c3711a90d8
JunhuiLee <861921648@qq.com>
2020-09-30 19:12:09 +0800

    分屏效果完善。

---
+++ Exermon2/Assets/Scripts/Core/UI/SplitCameraStateBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UI.BaseMapScene;
+
+namespace Core.UI {
+    /// <summary>
+    /// 基本状态行为
+    /// </summary>
+    public class SplitCameraStateBehaviour : BaseStateBehaviour {
+        /// <summary>
+        /// 内部变量
+        /// </summary>
+        BaseMapScene mapScene;
+        bool finish = false;
+
+        #region 初始化
+        protected override void setup(GameObject go) {
+            base.setup(go);
+            mapScene = gameObject.GetComponent<BaseMapScene>();
+        }
+        #endregion
+
+        /// <summary>
+        /// 状态进入
+        /// </summary>
+        protected override void onStatusEnter() {
+            base.onStatusEnter();
+
+        }
+
+        /// <summary>
+        /// 状态更新
+        /// </summary>
+        protected override void onStatusUpdate() {
+            base.onStatusUpdate();
+            if(!finish && stateInfo.normalizedTime > stateInfo.length + 0.5) {
+                Debug.Log(" normalizedTime: " + stateInfo.normalizedTime);
+                Debug.Log(" length: " + stateInfo.length);
+
+                finish = true;
+                mapScene.resetCamera();
+                Debug.Log("camera Rect1: " + mapScene.map2.camera.rect);
+            }
+            //Debug.Log("camera Rect2: " + mapScene.map2.camera.rect);
+        }
+
+        /// <summary>
+        /// 状态离开
+        /// </summary>
+        protected override void onStatusExit() {
+            base.onStatusExit();
+            finish = false;
+            Debug.Log("camera Rect3: " + mapScene.map2.camera.rect);
+        }
+    }
+
+}
--- Exermon2/Assets/Scripts/Scenes/BaseMapScene.cs
+++ Exermon2/Assets/Scripts/Scenes/BaseMapScene.cs
@@ -20,39 +20,99 @@ namespace UI.BaseMapScene {
 	/// <summary>
 	/// 地图场景基类
 	/// </summary>
-	[RequireComponent(typeof(AnimationExtend))]
+	[RequireComponent(typeof(AnimationExtend), typeof(Animator))]
 	public abstract class BaseMapScene : BaseScene {
 
 		/// <summary>
 		/// 分屏类型
 		/// </summary>
 		public enum SplitType {
-			Single, // 单屏
+			PresentSingle, // 现在单屏
+            PastSingle, //过去单屏
 			Both, // 双屏（左屏为过去，右屏为现在）
-			LeftMain, // 左屏为主
-			RightMain // 右屏为主
+			PastMain, // 左屏为主
+			PresentMain, // 右屏为主
 		}
 
 		/// <summary>
 		/// 外部组件设置
 		/// </summary>
 		public Map map1, map2;
+        public RenderTexture renderTexture;
+        public Canvas splitCanvas;
 
 		/// <summary>
 		/// 内部组件设置
 		/// </summary>
 		[RequireTarget]
 		protected new AnimationExtend animation;
+        [RequireTarget]
+        protected Animator animator;
 
-		#region 分屏控制
+        bool present = false;
+        bool switching = false;
 
-		/// <summary>
-		/// 分屏
-		/// </summary>
-		public void splitCamera(SplitType type) {
-			// TODO: 添加分屏动画代码
+        #region 分屏控制
+
+        /// <summary>
+        /// 分屏
+        /// </summary>
+        public void splitCamera(SplitType type) {
+            if (switching)
+                return;
+            if (type == SplitType.PresentSingle)
+                switchToPresent();
+            else if (type == SplitType.PastSingle)
+                switchToPast();
+            //map1.camera.targetTexture
+            animator.SetTrigger(type.ToString());
 		}
 
-		#endregion
-	}
+        void switchToPresent() {
+            present = true;
+            switching = true;
+            map1.camera.rect = new Rect(0, 0, 1, 1);
+            map2.camera.rect = new Rect(0, 0, 1, 1);
+            map1.camera.targetTexture = renderTexture;
+        }
+
+        void switchToPast() {
+            present = false;
+            switching = true;
+            map1.camera.rect = new Rect(0, 0, 1, 1);
+            map2.camera.rect = new Rect(0, 0, 1, 1);
+            map2.camera.targetTexture = renderTexture;
+        }
+
+        public void resetCamera() {
+            if (present) {
+                map2.camera.rect = new Rect(0, 0, 0, 1);
+                map1.camera.rect = new Rect(0, 0, 1, 1);
+                map1.camera.targetTexture = null;
+            }
+            else {
+                map1.camera.rect = new Rect(0, 0, 0, 1);
+                map2.camera.rect = new Rect(0, 0, 1, 1);
+                map2.camera.targetTexture = null;
+            }
+            switching = false;
+        }
+        #endregion
+
+        #region 测试
+        protected override void update() {
+            base.update();
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+                splitCamera(SplitType.PresentSingle);
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+                splitCamera(SplitType.PastSingle);
+            else if (Input.GetKeyDown(KeyCode.B))
+                splitCamera(SplitType.Both);
+            else if (Input.GetKeyDown(KeyCode.L))
+                splitCamera(SplitType.PastMain);
+            else if (Input.GetKeyDown(KeyCode.R))
+                splitCamera(SplitType.PresentMain);
+        }
+        #endregion
+    }
 }
